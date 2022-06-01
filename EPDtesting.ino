534d2942469c1036d3cc12eee1eda46a5a2f14e2
prototim <tim.serge@gmail.com>
2022-06-01 19:31:20 -0400

    Update EPDtesting.ino
     Minimum viable code. Screen shows starting switches which are set, reset functions, joystick  and gauge functions. Not going to implement the antenna pan/tilt due to 5V concerns.
    
    Updated the USBHost/src/Usb.h, which seems to fix the USB clashing. 
    
    #define USB_XFER_TIMEOUT		500	//was 5000    //USB transfer timeout in milliseconds, per section

--- EPDtesting.ino
+++ EPDtesting.ino
@@ -225,12 +225,6 @@ void setup()
 }
 
 
-/*void checkSwitches()
-  {
-
-
-  }
-  /**/
 
 
 
@@ -265,35 +259,35 @@ void updateSwitchScreen()
 {
 
   if (digitalRead(telemetrySwitch) == LOW) {
-    Serial.println("switch1");
+//    Serial.println("switch1");
     switchArray[0] = true;
   }
   if (digitalRead(oxygenValveSwitch) == LOW) {
-    Serial.println("switch2");
+//    Serial.println("switch2");
     switchArray[1] = true;
   }
   if (digitalRead(methaneValveSwitch) == LOW) {
-    Serial.println("switch3");
+//    Serial.println("switch3");
     switchArray[2] = true;
   }
   if (digitalRead(turboPumpSwitch) == LOW) {
-    Serial.println("switch4");
+//    Serial.println("switch4");
     switchArray[3] = true;
   }
   if (digitalRead(landerLockSwitch) == LOW) {
-    Serial.println("switch5");
+//    Serial.println("switch5");
     switchArray[4] = true;
   }
   if (digitalRead(ignitionEnabledSwitch) == LOW) {
-    Serial.println("switch6");
+//    Serial.println("switch6");
     switchArray[5] = true;
   }
   if (digitalRead(masterArmSwitch) == LOW) {
-    Serial.println("switch7");
+//    Serial.println("switch7");
     switchArray[6] = true;
   }
   if (digitalRead(initiateLaunchSwitch) == HIGH) {
-    Serial.println("switch8");
+//    Serial.println("switch8");
     switchArray[7] = true;
   }
   //  if(digitalRead(switchInput09) == LOW){ Serial.println("switch9");switchArray[8] = true;}
@@ -371,13 +365,7 @@ void updateKeyboardScreen()
   uint16_t x_input = 24;
   uint16_t y_input = 240;
 
-  //display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
-  // center the bounding box by transposition of the origin:
-  //uint16_t x = ((display.width() - tbw) / 2) - tbx;
-  //uint16_t y = ((display.height() - tbh) / 2) - tby;
-  //top left of screen
-  //display.setFullWindow();
-
+ 
   //remember to use increment of 8
   display.setPartialWindow(x_input - 24, y_input - 24, 376, 48);
   display.firstPage();
@@ -391,9 +379,6 @@ void updateKeyboardScreen()
 
   }
   while (display.nextPage());
-
-
-
 
 }
 
@@ -582,8 +567,8 @@ void panTilt()
       time_now = millis();
       //Move elevation gauge
       azimuthPos = azimuthPos + jogSpeed;
-//     Serial.print("Az Gauge Pos: ");
-//     Serial.print(azimuthPos);
+ //     Serial.print("Az Gauge Pos: ");
+ //     Serial.print(azimuthPos);
       analogWrite(azimuthGauge, azimuthPos);
 
       //Move pan servo
@@ -623,7 +608,7 @@ void panTilt()
 
       //If both gauges are lined up, then send signal to the Pi
       if (azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin) {
-        Serial.println("postition locked");
+//        Serial.println("postition locked");
         //antennaAligned = true;
       }
     }
@@ -650,7 +635,7 @@ void panTilt()
 
       //If both gauges are lined up, then send signal to the Pi
       if (azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin) {
-        Serial.println("postition locked");
+//        Serial.println("postition locked");
         //antennaAligned = true;
       }
     }
@@ -663,8 +648,8 @@ void panTilt()
       time_now = millis();
       //Move elevation gauge
       elevationPos = elevationPos - jogSpeed;
-      Serial.print("Ev Gague Pos: ");
-      Serial.print(elevationPos);
+//      Serial.print("Ev Gague Pos: ");
+//      Serial.print(elevationPos);
       analogWrite(elevationGauge, elevationPos);
 
       //Move pan servo
@@ -672,12 +657,12 @@ void panTilt()
       tilt.write(tiltPos);
       //delay(500);
 
-      Serial.print("   Servo Down: ");
-      Serial.println(tiltPos);
+//      Serial.print("   Servo Down: ");
+//      Serial.println(tiltPos);
 
       //If both gauges are lined up, then set a flag to true
       if (azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin) {
-        Serial.println("postition locked");
+//        Serial.println("postition locked");
         // antennaAligned = true;
       }
     }
@@ -690,123 +675,11 @@ void loop() {
 
   //attach USB task
   usb.Task();
+  
+  //call the pan and tilt servo function
   panTilt();
 
-  //updateSwitchScreen();
-
-
-  //
-  // //right-pan-azimuth
-  //  if(digitalRead(right) == LOW && azimuthPos <= servoMax && antennaAligned == false){
-  //    if( millis() > time_now + periodServo)
-  //    {
-  //      time_now = millis();
-  //      //Move elevation gauge
-  //      azimuthPos = azimuthPos + jogSpeed;
-  //      Serial.print("Az Gauge Pos: ");
-  //      Serial.print(azimuthPos);
-  //      analogWrite(azimuthGauge,azimuthPos);
-  //
-  //      //Move pan servo
-  //      panPos = panPos + jogSpeed;
-  //      pan.write(panPos);
-  //      //delay(500);
-  //      Serial.print("   Servo right: ");
-  //      Serial.println(panPos);
-  //
-  //      //If both gauges are lined up, then send signal to the Pi
-  //      if(azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin){
-  //       Serial.println("postition locked");
-  //       antennaAligned = true;
-  //      }
-  //    }
-  //   }
-  //
-  //
-  // //left-pan-azimuth
-  //  if(digitalRead(left) == LOW && azimuthPos >= servoMin && antennaAligned == false){
-  //    if( millis() > time_now + periodServo)
-  //    {
-  //      time_now = millis();
-  //      //Move elevation gauge
-  //      azimuthPos = azimuthPos - jogSpeed;
-  ////      Serial.print("Az Gauge Pos: ");
-  ////      Serial.print(azimuthPos);
-  //      analogWrite(azimuthGauge,azimuthPos);
-  //
-  //      //Move pan servo
-  //      panPos = panPos - jogSpeed;
-  //      pan.write(panPos);
-  //      //delay(500);
-  ////      Serial.print("   Servo left: ");
-  ////      Serial.println(panPos);
-  //
-  //
-  //      //If both gauges are lined up, then send signal to the Pi
-  //      if(azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin){
-  //       Serial.println("postition locked");
-  //       antennaAligned = true;
-  //      }
-  //    }
-  //   }
-  //
-  ////up-tilt-elevation
-  //  if(digitalRead(up) == LOW && elevationPos <= servoMax && antennaAligned == false){
-  //    if( millis() > time_now + periodServo)
-  //    {
-  //      time_now = millis();
-  //      //Move elevation gauge
-  //      elevationPos = elevationPos + jogSpeed;
-  ////      Serial.print("Ev Gauge Pos: ");
-  ////      Serial.print(elevationPos);
-  //      analogWrite(elevationGauge,elevationPos);
-  //
-  //      //Move pan servo
-  //      tiltPos = tiltPos + jogSpeed;
-  //      tilt.write(tiltPos);
-  //      //delay(500);
-  ////      Serial.print("  Servo Up: ");
-  ////      Serial.println(tiltPos);
-  //
-  //
-  //      //If both gauges are lined up, then send signal to the Pi
-  //      if(azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin){
-  //       Serial.println("postition locked");
-  //       antennaAligned = true;
-  //      }
-  //    }
-  //   }
-  //
-  ////down-tilt-elevation
-  //  if(digitalRead(down) == LOW && elevationPos >= servoMin && antennaAligned == false){
-  //    if( millis() > time_now + periodServo)
-  //    {
-  //      time_now = millis();
-  //      //Move elevation gauge
-  //      elevationPos = elevationPos - jogSpeed;
-  //      Serial.print("Ev Gague Pos: ");
-  //      Serial.print(elevationPos);
-  //      analogWrite(elevationGauge,elevationPos);
-  //
-  //      //Move pan servo
-  //      tiltPos = tiltPos - jogSpeed;
-  //      tilt.write(tiltPos);
-  //      //delay(500);
-  //
-  //      Serial.print("   Servo Down: ");
-  //      Serial.println(tiltPos);
-  //
-  //      //If both gauges are lined up, then set a flag to true
-  //      if(azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin){
-  //       Serial.println("postition locked");
-  //       antennaAligned = true;
-  //      }
-  //    }
-  //
-  //   }
-
-
-  //updateSwitchScreen();
+  
 
 }
 
@@ -832,216 +705,4 @@ void taskUpdate(int switchNum) {
   }
   while (display.nextPage());
 
-
 }
-
-void oxygenValve_ISR() {
-  oxygenValve_time = millis();
-  if (oxygenValve_time - last_oxygenValve_time > 250) {
-    taskUpdate(1);
-    //    updateSwitchScreen();
-    display.setRotation(0);
-    display.setFont(&FreeMonoBold9pt7b);
-    display.setTextColor(GxEPD_BLACK);
-    int16_t tbx, tby; uint16_t tbw, tbh;
-
-
-    uint16_t x_input = 24;
-    uint16_t y_input = 8;
-
-    //remember to use increment of 8
-    display.setPartialWindow(x_input, y_input - 8, 24, 152);
-    display.firstPage();
-    do
-    {
-      display.fillRect(x_input, y_input, 24, 8, GxEPD_BLACK);
-    }
-    while (display.nextPage());
-  }
-
-  last_oxygenValve_time = oxygenValve_time;
-}
-
-void methaneValve_ISR() {
-  methaneValve_time = millis();
-  if (methaneValve_time - last_methaneValve_time > 250) {
-    Serial.print("Interrupt2 ");
-    Serial.print(y++);
-    Serial.println();
-    last_methaneValve_time = methaneValve_time;
-  }
-}
-
-
-void turboPump_ISR() {
-  turboPump_time = millis();
-  if (turboPump_time - last_turboPump_time > 250) {
-    Serial.print("Interrupt3 ");
-    Serial.print(y++);
-    Serial.println();
-    last_turboPump_time = turboPump_time;
-  }
-}
-
-
-void landerLock_ISR() {
-  Serial.print("In lander lock");
-  landerLock_time = millis();
-  // if (landerLock_time - last_landerLock_time > 250){
-  display.setRotation(0);
-  display.setFont(&FreeMonoBold9pt7b);
-  display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby; uint16_t tbw, tbh;
-
-
-  uint16_t x_input = 24;
-  uint16_t y_input = 8;
-
-  //remember to use increment of 8
-  display.setPartialWindow(x_input, y_input - 8, 24, 152);
-  display.firstPage();
-  do
-  {
-    display.fillRect(x_input, y_input, 24, 8, GxEPD_BLACK);
-  }
-  while (display.nextPage());
-  //    last_landerLock_time = landerLock_time;
-  //    }
-}
-
-void ignitionEnabled_ISR() {
-  ignitionEnabled_time = millis();
-  if (ignitionEnabled_time - last_ignitionEnabled_time > 250) {
-    Serial.print("Interrupt5 ");
-    Serial.print(y++);
-    Serial.println();
-    last_ignitionEnabled_time = ignitionEnabled_time;
-  }
-}
-
-void masterArm_ISR() {
-  masterArm_time = millis();
-  if (masterArm_time - last_masterArm_time > 250) {
-    Serial.print("Interrupt6 ");
-    Serial.print(y++);
-    Serial.println();
-    last_masterArm_time = masterArm_time;
-  }
-}
-
-void telemetry_ISR() {
-  telemetry_time = millis();
-  if (telemetry_time - last_telemetry_time > 250) {
-    Serial.print("Interrupt7 ");
-    Serial.print(y++);
-    Serial.println();
-    last_telemetry_time = telemetry_time;
-  }
-}
-
-void initiateLaunch_ISR() {
-  initiateLaunch_time = millis();
-  if (initiateLaunch_time - last_initiateLaunch_time > 250) {
-    Serial.print("Interrupt8 ");
-    Serial.print(y++);
-    Serial.println();
-    last_initiateLaunch_time = initiateLaunch_time;
-  }
-}
-
-//joyUp-tilt-elevation
-void joyUp_ISR() {
-  Serial.print("UP!!");
-  // panTilt();
-  joyUp_time = millis();
-  if (joyUp_time - last_joyUp_time > 500) {
-
-    //Move elevation gauge
-    elevationPos = elevationPos + jogSpeed;
-    analogWrite(elevationGauge, elevationPos);
-
-    tiltPos = tiltPos + jogSpeed;
-    if (servoMin < tiltPos < servoMax) {
-      //Move tilt servo
-      Serial.print("Moving");
-      tilt.write(tiltPos);
-    }
-    last_joyUp_time = joyUp_time;
-    if (azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin) {
-      //antennaAligned();
-    }
-  }
-}
-
-//joyDown-tilt-elevation
-void joyDown_ISR() {
-  joyDown_time = millis();
-  if (joyDown_time - last_joyDown_time > 500) {
-
-    //Move elevation gauge
-    elevationPos = elevationPos - jogSpeed;
-    analogWrite(elevationGauge, elevationPos);
-
-    tiltPos = tiltPos - jogSpeed;
-    if (servoMin < tiltPos < servoMax) {
-      //Move tilt servo
-      tilt.write(tiltPos);
-    }
-    last_joyDown_time = joyDown_time;
-
-    if (azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin) {
-      //antennaAligned();
-    }
-  }
-}
-
-//joyLeft-pan-azimuth
-void joyLeft_ISR() {
-  joyLeft_time = millis();
-  if (joyLeft_time - last_joyLeft_time > 500) {
-
-    //Move azimuth gauge
-    azimuthPos = azimuthPos - jogSpeed;
-    analogWrite(azimuthGauge, azimuthPos);
-
-    panPos = panPos - jogSpeed;
-    if (servoMin < tiltPos < servoMax) {
-      //Move pan servo
-      pan.write(panPos);
-    }
-    last_joyLeft_time = joyLeft_time;
-
-    if (azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin) {
-      //antennaAligned();
-    }
-  }
-}
-
-//joyRight-pan-azimuth
-void joyRight_ISR() {
-  joyRight_time = millis();
-  if (joyRight_time - last_joyRight_time > 500) {
-
-    //Move azimuth gauge
-    azimuthPos = azimuthPos + jogSpeed;
-    analogWrite(azimuthGauge, azimuthPos);
-
-    panPos = panPos + jogSpeed;
-    if (servoMin < tiltPos < servoMax) {
-      //Move pan servo
-      pan.write(panPos);
-    }
-    last_joyRight_time = joyRight_time;
-
-    if (azimuthPos <= desiredAzimuthMax && azimuthPos >= desiredAzimuthMin && elevationPos <= desiredElevationMax && elevationPos >= desiredElevationMin) {
-      //antennaAligned();
-    }
-  }
-}
-
-// not using this code wihle the coprocessor exists
-//void antennaAligned()
-//{
-//    //TIMMMMM WRITE ME SOME CODE!!!
-//    Serial.println("postition locked");
-//}
